ce04a1c0bddc67071658c5132c44699ed46106ce
Tarun <154779714+tarunwrld@users.noreply.github.com>
2025-01-26 18:36:32 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+import uvicorn
+
+app = FastAPI()
+
+data_store = {}
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/fastapi/add_names/{ticker}")
+async def add_name(ticker: str):
+    try:
+        data_store[ticker] = ticker
+        json_data = [{"ticker": ticker} for ticker in data_store.values()]
+        return JSONResponse(content=json_data)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/fastapi/get-names")
+async def get_stored_data():
+    return JSONResponse(content={"stored_tickers": list(data_store.values())})
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8080)
